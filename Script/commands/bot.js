3b6922ff6789c7a91f1a7f65f80a1f4fc2fae0b8
Taohidislamtasin1234 <islamtaohid456@gmail.com>
2025-06-29 21:35:49 +0600

    Update bot.js

--- Script/commands/bot.js
+++ Script/commands/bot.js
@@ -50,9 +50,9 @@ return api.sendMessage("<আমি তোমাকে রাইতে মিস 
 
  
 
-if ((event.body.toLowerCase() == "😘") || (event.body.toLowerCase() == "😽")) { 
+if ((event.body.toLowerCase() == "🙋‍♀️") || (event.body.toLowerCase() == "😽")) { 
 
-return api.sendMessage("কিস দিস না তোর মুখে দূর গন্ধ কয়দিন ধরে দাঁত ব্রাশ করিস নাই🤬", threadID); 
+return api.sendMessage("তুমি মনে হয় আমার বস তাহসিনকে ভালোবাসো তাই ইমুজি দিতেছো তাই না,সমস্যা নাই গ্রুপে সবাই জানে তাহসিন বস আনেক ভদ্র ছেলে এত সহজে পটবে না,তুমি চালিয়ে যাও,তাহসিন বসের আইডির লিঙ্ক👉https://www.facebook.com/taohid.islam.tahosin , ইনবক্সে আবার যাইও না👻", threadID); 
 
 }; 
 
